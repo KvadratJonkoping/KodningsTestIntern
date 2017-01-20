f72f80b70ca1f0d6aa0f0f81bab7c6de3af3a992
Roland Heimdahl <roland@javalia.se>
2017-01-20 20:09:04 +0100

    Rätt startsiffra (#8)
    * första försöket
    
    * fel...
    
    * Starta med rätt nummer så blir det lite bättre

--- buurd/clojure1.8/src/clojure/kodnot.clj
+++ buurd/clojure1.8/src/clojure/kodnot.clj
@@ -20,14 +20,15 @@
 ;; hämta ut 10 bokstavslistor från 2 och uppåt genom att droppa de tre första
 ;; summera "bokstäverna" och ta ut modulus per lista
 ;; summera listan och ta ut modulus.
-(mod (reduce + (map to-mod10 (take 10 (drop 0 strings)))) 10)
+(mod (reduce + (map to-mod10 (take 10 (drop 3 strings)))) 10)
+
 
 ;; hämta ut 50 bokstavslistor från 2 och uppåt genom att droppa de tre första
 ;; summera "bokstäverna" och ta ut modulus per lista
 ;; summera listan och ta ut modulus.
-(mod(reduce + (map to-mod10 (take 50 (drop 0 strings)))) 10 )
+(mod(reduce + (map to-mod10 (take 50 (drop 3 strings)))) 10 )
 
 ;; hämta ut 1000 bokstavslistor från 2 och uppåt genom att droppa de tre första
 ;; summera "bokstäverna" och ta ut modulus per lista
 ;; summera listan och ta ut modulus.
-(mod(reduce + (map to-mod10 (take 1000 (drop 0 strings)))) 10)
+(mod(reduce + (map to-mod10 (take 1000 (drop 3 strings)))) 10)
